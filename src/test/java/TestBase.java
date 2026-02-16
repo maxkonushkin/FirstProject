6059abfd009efadcb2e32f391d36e205be4f4af3
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-02-16 12:19:54 +0400

    add SelenideLogger

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,8 +1,16 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
     @Story("Открываем сайт банка")
     @BeforeAll
