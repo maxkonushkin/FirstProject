7012f309b1f938ed19f8b53f35b94f3943a207cf
KonushkinMaksim <mkonushkinqa@gmail.com>
2026-02-16 12:09:36 +0400

    add Steps

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,8 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
+    @Step("Открываем сайт банка")
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
